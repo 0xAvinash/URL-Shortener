9b538d3b878187230029959fae9c0eafbc7a3b1c
Avinash <127190931+Avinash-Vadlamudi@users.noreply.github.com>
2026-04-14 17:08:55 +0530

    added a `uint64` to `id` field

--- internal/models/url.go
+++ internal/models/url.go
@@ -3,8 +3,8 @@ package models
 import "gorm.io/gorm"
 
 type URL struct {
-	ID        uint   `json:"id" gorm:"primaryKey"`
-	ShortCode string `json:"short_code" gorm:"uniqueIndex"`
-	LongURL   string `json:"long_url"`
+	ID        uint64   `json:"id" gorm:"primaryKey"`
+	ShortCode string `json:"short_code" gorm:"uniqueIndex;size:10"`
+	LongURL   string `json:"long_url" gorm:"type:text"`
 	gorm.Model
 }
