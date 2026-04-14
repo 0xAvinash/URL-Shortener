c3e7bbedf004ce91c6e5329a66adc1f919f33709
Avinash <127190931+Avinash-Vadlamudi@users.noreply.github.com>
2026-04-14 17:09:30 +0530

    created a `clicks` table to keep track of the URL clicks

---
+++ internal/models/clicks.go
@@ -0,0 +1,11 @@
+package models
+
+import "gorm.io/gorm"
+
+type Clicks struct {
+	ID uint64 `json:"id"`
+	ShortCode string `json:"short_code"`
+	IP string `json:"ip_address"`
+	UserAgent string `json:"user_agent" gorm:"type:text"`
+	gorm.Model
+}
