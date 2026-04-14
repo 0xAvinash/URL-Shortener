eb3502f0bc0c5fff1297426bd7024a13ae9f6ad2
Avinash <127190931+Avinash-Vadlamudi@users.noreply.github.com>
2026-04-14 17:11:57 +0530

    just issued a `go fmt ./...` command for better formatting

--- internal/models/clicks.go
+++ internal/models/clicks.go
@@ -3,9 +3,9 @@ package models
 import "gorm.io/gorm"
 
 type Clicks struct {
-	ID uint64 `json:"id"`
+	ID        uint64 `json:"id"`
 	ShortCode string `json:"short_code"`
-	IP string `json:"ip_address"`
+	IP        string `json:"ip_address"`
 	UserAgent string `json:"user_agent" gorm:"type:text"`
 	gorm.Model
 }
--- internal/models/url.go
+++ internal/models/url.go
@@ -3,7 +3,7 @@ package models
 import "gorm.io/gorm"
 
 type URL struct {
-	ID        uint64   `json:"id" gorm:"primaryKey"`
+	ID        uint64 `json:"id" gorm:"primaryKey"`
 	ShortCode string `json:"short_code" gorm:"uniqueIndex;size:10"`
 	LongURL   string `json:"long_url" gorm:"type:text"`
 	gorm.Model
