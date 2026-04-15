e1a7e339fd8117b078499f8a5c827e38653bfd2a
Avinash <127190931+Avinash-Vadlamudi@users.noreply.github.com>
2026-04-15 12:55:53 +0530

    finally made the app working

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,104 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+	"time"
+	"url-shortener/internal/database"
+	"url-shortener/internal/server"
+
+	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
+	"github.com/gin-gonic/gin"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+
+func initKafka() *kafka.Producer {
+    producer, err := kafka.NewProducer(&kafka.ConfigMap{
+        "bootstrap.servers": "localhost:9092",
+    })
+    if err != nil {
+        log.Fatal("failed to create kafka producer:", err)
+    }
+
+    return producer
+}
+
+
+func startgRPC() {
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	grpcServer := grpc.NewServer()
+
+	srv := server.NewServer(database.DB, database.RedisClient, initKafka(), "click-events")
+
+	server.RegisterURLServiceServer(grpcServer, srv)
+
+	log.Println("gRPC server running on :50051")
+
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
 
 func main() {
-	fmt.Println("Initialising the code base")
+	r := gin.Default()
+
+	// Initialize DB
+	database.ConnectDB()
+	database.NewRedis()
+
+	go startgRPC()
+	time.Sleep(1 * time.Second)
+
+	// gRPC connection
+	conn, err := grpc.Dial(
+		"localhost:50051",
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	)
+	if err != nil {
+		log.Fatal("failed to connect to gRPC server:", err)
+	}
+	defer conn.Close()
+
+	// Create gRPC client
+	client := server.NewURLServiceClient(conn)
+
+	// Route
+	r.POST("/shorten", func(c *gin.Context) {
+		var reqBody struct {
+			URL string `json:"url"`
+		}
+
+		if err := c.ShouldBindJSON(&reqBody); err != nil {
+			c.JSON(400, gin.H{"error": "invalid request"})
+			return
+		}
+
+		// Create context (IMPORTANT)
+		ctx := context.Background()
+
+		// Call gRPC method
+		resp, err := client.Shorten(ctx, &server.ShortenRequest{
+			LongUrl: reqBody.URL,
+		})
+		if err != nil {
+			c.JSON(500, gin.H{"error": err.Error()})
+			return
+		}
+
+		c.JSON(200, gin.H{
+			"short_url": resp.ShortCode,
+		})
+	})
+
+	fmt.Println("Server is running on localhost:3000")
+
+	r.Run(":3000")
 }
