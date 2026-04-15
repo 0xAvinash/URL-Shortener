b1a443f70946f0535e77d03d5502d2debfb4413b
Avinash <127190931+Avinash-Vadlamudi@users.noreply.github.com>
2026-04-15 19:08:20 +0530

    added click_count to the url table

--- internal/models/url.go
+++ internal/models/url.go
@@ -3,8 +3,9 @@ package models
 import "gorm.io/gorm"
 
 type URL struct {
-	ID        uint64 `json:"id" gorm:"primaryKey"`
-	ShortCode string `json:"short_code" gorm:"uniqueIndex;size:32"`
-	LongURL   string `json:"long_url" gorm:"type:text"`
+	ID         uint64 `json:"id" gorm:"primaryKey"`
+	ShortCode  string `json:"short_code" gorm:"uniqueIndex;size:32"`
+	LongURL    string `json:"long_url" gorm:"type:text"`
+	ClickCount uint64 `json:"click_count"`
 	gorm.Model
 }
