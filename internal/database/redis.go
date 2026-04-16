12456ef6b7e634b273a4d5e77e5daa3282abea21
Avinash <127190931+Avinash-Vadlamudi@users.noreply.github.com>
2026-04-16 11:57:57 +0530

    made changes to redis host: added `redis` server name to the project

--- internal/database/redis.go
+++ internal/database/redis.go
@@ -2,6 +2,8 @@ package database
 
 import (
 	"context"
+	"fmt"
+	"os"
 
 	"github.com/redis/go-redis/v9"
 )
@@ -11,8 +13,11 @@ var Ctx = context.Background()
 var RedisClient *redis.Client
 
 func NewRedis() *redis.Client {
+	redis_host := os.Getenv("REDIS_HOST")
+
+	host := fmt.Sprintf("%s:6379", redis_host)
 	RedisClient = redis.NewClient(&redis.Options{
-		Addr: "localhost:6379",
+		Addr: host,
 	})
 
 	return RedisClient
